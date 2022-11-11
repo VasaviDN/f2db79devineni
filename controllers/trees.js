ec814421a2505c97350da626c496e1dfe70eedf5
VasaviDN <vasavi.vzf95@gmail.com>
2022-11-11 12:19:02 -0600

    Updated code for put

--- controllers/trees.js
+++ controllers/trees.js
@@ -60,3 +60,36 @@ exports.tree_delete = function(req, res) {
 exports.tree_update_put = function(req, res) { 
     res.send('NOT IMPLEMENTED: Tree update PUT' + req.params.id); 
 }; 
+
+// for a specific tree. 
+exports.tree_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await tree.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
+
+// Handle Costume update form on PUT. 
+exports.tree_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await tree.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.tree_name)  
+               toUpdate.tree_name = req.body.tree_name; 
+        if(req.body.size) toUpdate.size = req.body.size; 
+        if(req.body.age) toUpdate.age = req.body.age; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
--- routes/trees.js
+++ routes/trees.js
@@ -5,4 +5,7 @@ var router = express.Router();
 /* GET trees */ 
 router.get('/', tree_controlers.tree_view_all_Page ); 
 
+// GET request for one costume. 
+router.get('/tree/:id', tree_controlers.tree_detail); 
+
 module.exports = router;
