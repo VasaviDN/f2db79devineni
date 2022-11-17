04af72f48141f8c417bcd0eb0c303d6a5b8fe16e
VasaviDN <vasavi.vzf95@gmail.com>
2022-11-16 21:03:42 -0600

    updated update and delete code

--- controllers/trees.js
+++ controllers/trees.js
@@ -115,3 +115,47 @@ exports.tree_view_one_Page = async function(req, res) {
         res.send(`{'error': '${err}'}`); 
     } 
 }; 
+
+// Handle building the view for creating a costume. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.tree_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('treecreate', { title: 'Tree Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle building the view for updating a costume. 
+// query provides the id 
+exports.tree_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await tree.findById(req.query.id) 
+        //
+        console.log(result)
+        res.render('treeupdate', { title: 'Tree Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle a delete one view with id from query 
+exports.tree_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await tree.findById(req.query.id) 
+        res.render('treedelete', { title: 'Tree Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
--- routes/trees.js
+++ routes/trees.js
@@ -6,9 +6,18 @@ var router = express.Router();
 router.get('/', tree_controlers.tree_view_all_Page ); 
 
 // GET request for one costume. 
-//router.get('/tree/:id', tree_controlers.tree_detail); 
+router.get('/tree/:id', tree_controlers.tree_detail); 
 
 /* GET detail Tree page */ 
 router.get('/detail', tree_controlers.tree_view_one_Page); 
+
+/* GET create costume page */ 
+router.get('/create', tree_controlers.tree_create_Page); 
+
+/* GET create update page */ 
+router.get('/update', tree_controlers.tree_update_Page); 
+
+/* GET delete costume page */ 
+router.get('/delete', tree_controlers.tree_delete_Page); 
 
 module.exports = router;
